b634708e1b34ca14f09b10dcb61d9e70914a39a3
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2024-05-05 16:52:10 +0300

    Update

--- src/js/Class/MainCharacter.js
+++ src/js/Class/MainCharacter.js
@@ -1,20 +1,19 @@
 export default class Character {
     constructor (name, type) {
-        const typesOfPlayers = ['Bowman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie']
+        const typesOfPlayers = ['Bowman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie'];
         if((name.length < 2) || (name.length > 10)) {
-            throw new Error('Не верно')
-        };
+            throw new Error('Не верно');
+        }
 
         if(!typesOfPlayers.includes(type)) {
-            throw new Error('Не верно')
-        };
+            throw new Error('Не верно');
+        }
 
-        this.name = name,
-        this.type = type,
-        this.health = 0,
-        this.level = 1,
-        this.attack = '',
-        this.defence = ''
-
+        this.name = name;
+        this.type = type;
+        this.health = 0;
+        this.level = 1;
+        this.attack = '';
+        this.defence = '';
     };
 };
