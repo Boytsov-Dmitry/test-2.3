7cc0993fd7b203d0dde120a226f1125bb91d8e3c
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2024-05-05 16:53:39 +0300

    Update

--- src/js/Class/MainCharacter.js
+++ src/js/Class/MainCharacter.js
@@ -13,7 +13,7 @@ export default class Character {
         this.type = type;
         this.health = 100;
         this.level = 1;
-        this.attack = '';
-        this.defence = '';
+        this.attack = undefined;
+        this.defence = undefined;
     };
 };
